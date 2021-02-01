f254d748610c70d6747a13549129bd74a9b519fb
devtill2yeah <74370134+devtill2yeah@users.noreply.github.com>
2021-02-01 23:39:19 +0100

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,19 @@
+
+$( "#mehr Erfahren" ).click(function() {
+  //alert("Mehr erfahren... angeklickt.");
+  $("#mehr anzeigen").text(".");
+  $("#mehr Erfahren").hide();
+});
+
+$( "#Impressum" ).click(function() {
+  //alert("Impressum... angeklickt.");
+  $("#mehr anzeigen").text("Dies ist das Verzeichnis zur rechtlichen Legitmität.");
+  $("#Impressum").hide();
+});
+
+$( "#socials" ).click(function() {
+  //alert("Socials... angeklickt.");
+  $("#mehr anzeigen").text("Hier finden Sie unsere Social Media Kanäle, über die Sie uns, das Team des Hogwarts Minecraft Projekts jederzeit bei Fragen erreichen können.");
+  $("#Socials").hide();
+});
+
